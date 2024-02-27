9e6e62bc7b5b6e2ae91eabfc28741e042b0d82d9
Timur Alekseev <tdalexeev@gmail.com>
2024-02-27 16:24:41 +0000

    add docker, separate files

--- services/web/app.py
+++ services/web/app.py
@@ -14,6 +14,7 @@ app.wsgi_app = ProxyFix(
 app.config.from_object(Config)
 Session(app)
 
+
 @app.route('/')
 def index():
     auth_manager = create_spotify_auth_manager(session)
@@ -25,14 +26,17 @@ def index():
         return render_template('signin.html', auth_url=auth_url)
     return render_template('index.html')
 
+
 @app.route('/sign_out')
 def sign_out():
     session.pop("token_info", None)
     return redirect('/')
 
+
 @app.route('/recommendations')
 def recommendations():
     return redirect('/')
+
 
 @app.route('/myplaylists')
 def myplaylists():
@@ -42,6 +46,7 @@ def myplaylists():
     spotify = get_spotify_client(auth_manager)
     user_playlists = get_currentuser_playlists(spotify, sort_by='name', ascending=True)
     return render_template('my_playlists.html', user_playlists=user_playlists)
+
 
 @app.route('/daily_mix_generator')
 def daily_mix_generator():
@@ -54,25 +59,6 @@ def daily_mix_generator():
     sorted_playlists = sorted(filtered_playlists, key=lambda x: x['name'].lower())
     return render_template('daily_mix_generator.html', playlists=sorted_playlists)
 
-# @app.route('/create_playlist', methods=['GET', 'POST'])
-# def create_playlist():
-#     auth_manager = create_spotify_auth_manager(session)
-#     if not auth_manager.validate_token(auth_manager.cache_handler.get_cached_token()):
-#         return redirect('/')
-#     if request.method == 'POST':
-#         playlist_name = request.form.get('playlist_name')
-#         overwrite = request.form.get('overwrite')
-#         if playlist_name:
-#             try:
-#                 spotify = get_spotify_client(auth_manager)
-#                 create_success, new_playlist_id  = create_new_playlist(spotify, playlist_name, overwrite)
-#                 return render_template('create_playlist.html', playlist_name=playlist_name, create_success=create_success, overwrite=overwrite)
-#             except Exception as e:
-#                 create_success = False
-#                 return render_template('create_playlist.html', playlist_name=playlist_name, create_success=create_success, overwrite=overwrite)
-#         else:
-#             return "Please provide a valid playlist name."
-#     return render_template('create_playlist.html')
 
 @app.route('/create_playlist', methods=['GET', 'POST'])
 def create_playlist():
@@ -98,6 +84,7 @@ def create_playlist():
         except Exception as e:
             return json.dumps({ "success": False, "message": str(e) }), 500
     return render_template('create_playlist.html')
+
 
 @app.route('/create_daily_mix', methods=['POST'])
 def create_daily_mix():
@@ -129,8 +116,6 @@ def create_daily_mix():
             return json.dumps({ "success": False, "message": str(e) }), 500
 
 
-
-
 @app.route('/current_user')
 def current_user():
     auth_manager = create_spotify_auth_manager(session)
@@ -138,6 +123,7 @@ def current_user():
         return redirect('/')
     spotify = get_spotify_client(auth_manager)
     return spotify.current_user()
+
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True, port=int(os.environ.get("PORT", 5000)))
