23779a54d94327a7567cb3d3ca5d04809e3b51ac
Timur Alekseev <tdalexeev@gmail.com>
2024-09-18 15:28:00 +0000

    add prometheus metrics

--- services/web/app.py
+++ services/web/app.py
@@ -9,7 +9,10 @@ from config import Config
 
 from werkzeug.middleware.proxy_fix import ProxyFix
 
+from prometheus_flask_exporter import PrometheusMetrics
+
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 app.wsgi_app = ProxyFix(
     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )
